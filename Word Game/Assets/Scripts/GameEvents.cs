4e571d7f19f55aabbf123833baa10ba663f9c119
walterlp <walterlp10@hotmail.com>
2021-10-12 01:06:49 -0300

    Mecanica pt1
    seleção de letra a letra adicionada;
    -> mudar para seleção continua;

---
+++ Word Game/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvents 
+{
+    public delegate void EnableSquareSelection();
+    public static event EnableSquareSelection OnEnableSquareSelection;
+    public static void EnableSquareSelectionMethod()
+    {
+        if (OnEnableSquareSelection != null)
+            OnEnableSquareSelection();
+    }
+
+    //-------------------------------------------------------------------------
+
+    public delegate void DisableSquareSelection();
+    public static event DisableSquareSelection OnDisableSquareSelection;
+    public static void DisebleSquareSelectionMethod()
+    {
+        if (OnDisableSquareSelection != null)
+            OnDisableSquareSelection();
+    }
+
+    //------------------------------------------------------------------------
+
+    public delegate void SelectSquare(Vector3 position);
+    public static event SelectSquare OnSelectSquare;
+    public static void SelectSquareMethod(Vector3 position)
+    {
+        if (OnSelectSquare != null)
+            OnSelectSquare(position);
+    }
+
+    //------------------------------------------------------------------------
+
+
+    public delegate void CheckSquare(string letter, Vector3 squarePosition, int squareIndex);
+    public static event CheckSquare OnCheckSquare;
+    public static void CheckSquareMethod(string letter, Vector3 squarePosition, int squareIndex)
+    {
+        if (OnCheckSquare != null)
+            OnCheckSquare(letter, squarePosition, squareIndex);
+    }
+
+    //------------------------------------------------------------------------
+
+
+    public delegate void ClearSelection();
+    public static event ClearSelection OnClearSelection;
+    public static void ClearSelectionMethod()
+    {
+        if (OnClearSelection != null)
+            OnClearSelection();
+    }
+
+}
--- Word Game/Assets/Scripts/GridSquare.cs
+++ Word Game/Assets/Scripts/GridSquare.cs
@@ -12,10 +12,59 @@ public class GridSquare : MonoBehaviour
 
     private SpriteRenderer _displayImage;
 
+    private bool _selected;
+    private bool _clicked;
+    private int _index;
+
+    public void setIndex(int index)
+    {
+        _index = index;
+    }
+    public int GetIndex()
+    {
+        return _index;
+    }
+
     void Start()
     {
+        _selected = false;
+        _clicked = false;
         _displayImage = GetComponent<SpriteRenderer>();
 
+
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnEnableSquareSelection += OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection += OnDisableSquareSelection;
+        GameEvents.OnSelectSquare += OnSelectSquare;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnEnableSquareSelection -= OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection -= OnDisableSquareSelection;
+        GameEvents.OnSelectSquare -= OnSelectSquare;
+    }
+
+    public void OnEnableSquareSelection()
+    {
+        _clicked = true;
+        _selected = false;
+    }
+    public void OnDisableSquareSelection()
+    {
+        _selected = false;
+        _clicked = false;
+
+    }
+    public void OnSelectSquare(Vector3 position)
+    {
+        if (this.gameObject.transform.position == position)
+        {
+            _displayImage.sprite = _selectedLetterData.image;
+        }
     }
     public void SetSprite(AlphabetData.LetterData normalLetterData, AlphabetData.LetterData selectedLetterData, AlphabetData.LetterData correctLetterData)
     {
@@ -26,4 +75,34 @@ public class GridSquare : MonoBehaviour
         GetComponent<SpriteRenderer>().sprite = _normalLetterData.image;
     }
 
+
+
+    private void OnMouseDown()
+    {
+        OnEnableSquareSelection();
+        GameEvents.EnableSquareSelectionMethod();
+        CheckSquare();
+        _displayImage.sprite = _selectedLetterData.image;
+    }
+
+    private void OnMouseEnter()
+    {
+        CheckSquare();
+    }
+
+    private void OnMouseUp()
+    {
+        GameEvents.ClearSelectionMethod();
+        GameEvents.DisebleSquareSelectionMethod();
+    }
+
+
+    public void CheckSquare()
+    {
+        if(_selected == false && _clicked == true)
+        {
+            _selected = true;
+            GameEvents.CheckSquareMethod(_normalLetterData.letter, gameObject.transform.position, _index);
+        }
+    }
 }
